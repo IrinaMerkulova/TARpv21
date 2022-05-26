5dbfab52a4d875ac63e0c7d3d64337697af4bb76
rozhkov <artjom.rozhkov123@gmail.com>
2022-05-26 11:20:43 +0300

    select29.sql on lisatud

---
+++ select29.sql
@@ -0,0 +1,9 @@
+select Gender, City, sum(cast(Salary as int)) as TotalSalary, 
+count (Id) as [Total Employee(s)]
+from Employees
+group by Gender, City;
+select Gender, City, sum(cast(Salary as int)) as TotalSalary, 
+count (Id) as [Total Employee(s)]
+from Employees
+where Gender = 'Male'
+group by Gender, City
