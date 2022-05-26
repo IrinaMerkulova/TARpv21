04582ad43d0a7b5792b9d145bc4e6387061390d2
rozhkov <artjom.rozhkov123@gmail.com>
2022-05-26 12:15:28 +0300

    select34.sql on lisatud

---
+++ select34.sql
@@ -0,0 +1,4 @@
+﻿
+select datename(day, '2022-04-18 13:30:55.866') -- annab vastukses 18
+select datename(weekday, '2022-04-18 13:30:55.866')  -- annab vastuseks esmasp�ev
+select datename(month, '2022-04-18 13:30:55.866')  --- annab vastuseks Aprill
