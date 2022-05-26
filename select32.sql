05662a162b9c3ca8087585dd2ed5f31c80f277ec
rozhkov <artjom.rozhkov123@gmail.com>
2022-05-26 12:14:14 +0300

    select32.sql on lisatud

---
+++ select32.sql
@@ -0,0 +1,4 @@
+﻿select CURRENT_TIMESTAMP, 'CURRENT_TIMESTAMP' --- aja p�ring
+select SYSDATETIME(), 'SYSDATETIME' --- veel t�psem aja p�ring
+select SYSDATETIMEOFFSET(), 'SYSDATETIMEOFFSET' -- t�pne aeg koos ajalise nihkega UTC suhtes
+select GETUTCDATE(), 'GETUTCDATE' --- UTC aeg
