b857852a68faaf45b6e5e65f134f3cae9e2f6c93
AleksandrKastanie <aleksandr.kastanie@gmail.com>
2022-06-02 09:18:54 +0300

    select 5 - inimesed, kelle linn algab n-ga , Inimesed kelle mail sisaldab @

--- Select_5.sql
+++ Select_5.sql
@@ -1,2 +1,4 @@
+-- отображает людей, чей город начинается на букву n
 select * from Person where City like 'n%'
+-- отображает людей, в чьем маиле есть @
 select * from Person where Email like '%@%'
