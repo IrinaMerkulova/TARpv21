ca82142984a217c92cda518df4da77d17ced0a9b
IljaTrunov <Iljatrunov123@gmail.com>
2022-06-02 10:55:04 +0300

    indexemployee ja department join ilma palka

---
+++ ilmapalkviewDepartment.sql
@@ -0,0 +1,7 @@
+﻿create view vEmployeesDataExceptSalary
+as
+select EmployeeIndex.Id, Name, Gender, DepartmentName
+from EmployeeIndex
+join Department
+on EmployeeIndex.DeptNameId = Department.Id
+--без зарплаты показ
