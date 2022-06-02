ba41b62d60ee6a3f24bec06d2605cf53b7a89616
IljaTrunov <Iljatrunov123@gmail.com>
2022-06-02 10:51:40 +0300

    indexemployee ja department join

---
+++ createviewDepartament.sql
@@ -0,0 +1,9 @@
+﻿create view vITDepartment_Employees
+as 
+select EmployeeIndex.Id, Name, Salary, Gender, DepartmentName
+from EmployeeIndex
+join Department
+on EmployeeIndex.DeptNameId = Department.Id
+where Department.DepartmentName = 'IT'
+
+--создает вид в котором будет департамент, имя человека и зп и пол
