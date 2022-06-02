64455e4dd0baa9f1c2dd1f2ba4735bc644647fb4
AleksandrKastanie <aleksandr.kastanie@gmail.com>
2022-06-02 09:24:29 +0300

    select 7 - Inimesed kelle mail lõpeb on .com-iga

--- Select_7.sql
+++ Select_7.sql
@@ -1,1 +1,2 @@
-select * from Person where Email like '_@_.com'
+-- показывает людей, у которых почта заканчивается на  .com
+select * from Person where Email like '_@_.com'select * from Person where Email like '_@_.com'
