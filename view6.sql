07076f886a604e48c2a69ab955391d38a66fa742
rozhkov <artjom.rozhkov123@gmail.com>
2022-06-02 10:45:45 +0300

    view6.sql on lisatud

---
+++ view6.sql
@@ -0,0 +1,12 @@
+﻿create view vTotalSalesByProduct
+with schemabinding  -- seob k�sitletavaid tabeleid ja ei luba muuta igatepidi
+as
+select Name,
+SUM(ISNULL((QuantitySold * UnitPrice), 0)) as TotalSales,
+count_big(*) as TotalTransactions
+from dbo.ProductSales
+join dbo.Product --kindlasti panna tabeli ette dbo
+on Product.Id = ProductSales.Id
+group by Name
+
+select * from vTotalSalesByProduct
