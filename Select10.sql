e77b30239f08e89eea7e5e4ed969c0c2561055bf
ArtjomVolkov <artjomvolk123@gmail.com>
2022-06-02 09:28:25 +0300

    select10 - näitab kõiki, kes elavad Tallinna või Narva linnas ja on üle 40 aasta vanad

--- Select10.sql
+++ Select10.sql
@@ -1,2 +1,3 @@
+-- показывает всю таблицу и показывает всех кто живет в городе Таллинн либо Нарва и возраст больше 40 лет
 select * from Person where (City = 'Tallinn' or City = 'Narva')
 and Age >= 40
