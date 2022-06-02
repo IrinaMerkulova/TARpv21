9629db4c1936133d508bbc6546fd9cf6071df9c4
AlinaKolomoiets <alinakolomoits@gmail.com>
2022-06-02 09:38:11 +0300

    Näitab inimesi, kes elavad Maardus või Tallinnas ja on vanemad kui 40

--- SQLQuery8.sql
+++ SQLQuery8.sql
@@ -1,5 +1,6 @@
+--Показывает людей живущих в маарду или таллине
 select * from Person where (City = 'Tallinn' or City = 'Maardu') 
-
+ --Показывает людей живущих в маарду или таллине и старше 40
 select * from Person where (City = 'Tallinn' or City = 'Maardu')
 and Age >= 40
 
