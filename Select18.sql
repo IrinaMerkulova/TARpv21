91d38cfe38da5e64d57f7b6ea714c1af2915d064
vlademirGužov <vlademirguzov@gmail.com>
2022-06-02 09:45:15 +0300

    select18-19 komenteeritud

--- Select18.sql
+++ Select18.sql
@@ -1,1 +1,2 @@
+--показать минимум и преобразовать age в int из таблицы person
 select min(cast(Age as int)) from Person
--- Select19.sql
+++ Select19.sql
@@ -1,3 +1,4 @@
+--показать максимум и преобразовать age в инт из таблицы person
 select max(cast(Age as int)) from Person
-
+--показать city,сумировать и преобразовать age в инт с названием totalage из таблицы person  групировать по city
 select City, sum(cast(Age as int)) as TotalAge from Person group by City
