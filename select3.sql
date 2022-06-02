42106d9c9c631909f91d66c58bdd4f6801d56b8f
German <germanvasiljev2.0@germanprogrammer>
2022-06-02 09:24:40 +0300

    Select3 - Kes on 100, 50 või 20 aastat vana

--- select3.sql
+++ select3.sql
@@ -1,3 +1,5 @@
-select *from Person where Age = 100 or 
+-- Выводит возраст 100,50 или 20
+select * from Person where Age = 100 or 
 Age = 50 or Age = 20
+-- Выводит возраст 100,50 или 20
 select * from Person where Age in (100, 50, 20)
