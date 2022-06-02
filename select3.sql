9ec5b004e521d0fd06fead6499a2292a58bc86b0
ArturLink <arturlink04@gmail.com>
2022-06-02 09:10:47 +0300

    select 3 - kes on 23 või 24 või 34 aastat vana

--- select3.sql
+++ select3.sql
@@ -1,3 +1,5 @@
+-- Выводит людей с возрастом или 23  или 32 или 24
 select *from Person where Age = 23 or 
 Age = 34 or Age = 24
+-- Выводит людей с возрастом или 23  или 32 или 24
 select * from Person where Age in (23, 34, 24)
