e15d5f167e601c0ed519ea740cbab8e10d185f2b
{topolja} <{timothytopoly@gmail.com}>
2022-06-02 09:33:35 +0300

    select4 - vaatame kes elab Gothamis ja kes ei ela Gothamis

---
+++ select4.sql
@@ -0,0 +1,5 @@
+﻿-- все кто живут в городе Gotham
+select * from Person where City = 'Gotham'
+-- все кто не живут в городе Gotham
+select * from Person where City <> 'Gotham'
+select * from Person where City != 'Gotham'
