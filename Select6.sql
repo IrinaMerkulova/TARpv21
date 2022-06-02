96d8b414dc1819a9142b1f1fa83e6a9715482c76
vlademirGužov <vlademirguzov@gmail.com>
2022-06-02 09:30:04 +0300

    select6-7 komenteeritud

--- Select6.sql
+++ Select6.sql
@@ -1,1 +1,2 @@
+--показать все из таблицы peson где email не содержит @
 select * from Person where Email not like '%@%'
--- Select7.sql
+++ Select7.sql
@@ -1,1 +1,2 @@
+--показать все из таблицы персон где email равен _@_.com
 select * from Person where Email like '_@_.com'
