24fbac1278d877b58a58ec07d93fa658572b55c3
vlademirGužov <vlademirguzov@gmail.com>
2022-06-02 09:41:55 +0300

    select16-17 komenteeritud

--- Select16.sql
+++ Select16.sql
@@ -1,2 +1,3 @@
+----показать все из таблицы person сортировать по age
 select * from Person order by cast(Age as int)
 select * from Person order by Age
--- Select17.sql
+++ Select17.sql
@@ -1,1 +1,2 @@
+--показать сумму и преобразовать age  в int из таблицы person
 select sum(cast(Age as int)) from Person
