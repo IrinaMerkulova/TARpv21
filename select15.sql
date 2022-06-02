a3c6a62f0c73910293ec570b8a1a2f44a648ade6
German <germanvasiljev2.0@germanprogrammer>
2022-06-02 10:23:10 +0300

    select15 - Sorteeritud vanuse järgi (madalaimast suurimani), sorteeritud vanuse järgi (kõrgeimast väikseimani)

--- select15.sql
+++ select15.sql
@@ -1,2 +1,4 @@
+-- Сортировка по возрасту(С меньшего к большему)
 select * from Person order by cast(Age as int)
+-- Сортировка по возрасту(С боьльшего к меньшему)
 select * from Person order by Age
