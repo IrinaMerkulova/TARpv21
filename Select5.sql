3a1a7cd3a4a3c45ff08bebc0ecce2863d9f4cbfe
ArtjomVolkov <artjomvolk123@gmail.com>
2022-06-02 09:17:54 +0300

    select5 - näitab linna, kus on täht N ja näitab e-kirju, kus on @

--- Select5.sql
+++ Select5.sql
@@ -1,2 +1,4 @@
+-- выводит всю таблицу и показывает город где есть буква N
 select * from Person where City like 'n%'
+-- выводит всю таблицу и показывает емайлы где есть @
 select * from Person where Email like '%@%'
