53b4eebcfd924e4290200a4e555a2dfec76ddaea
Edgar Neverovski <edgarneverovskij@gmail.com>
2022-06-02 09:44:15 +0300

    Select18 - kõige vanem isik

--- SQL запросы/SQLQuery18.sql
+++ SQL запросы/SQLQuery18.sql
@@ -1,3 +1,3 @@
 -- 18. kõige vanem isik
-select max(cast(Age as int)) from Person
+select max(cast(Age as int)) from Person 
 select City, sum(cast(Age as int)) as TotalAge from Person group by City
