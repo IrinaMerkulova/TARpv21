67d8d8d9c183413236dc3ba893da4e25d78acf05
Mihhail Lastovski <lastovskim@gmail.com>
2022-06-02 10:18:09 +0300

    Select 22 - näitab vanuse ja kirjete summat, kui GenderId = 2 ning sorteerib GenderId ja City järgi

--- Select22.sql
+++ Select22.sql
@@ -1,3 +1,4 @@
+--Показывает сумму возраста и колличество записей, если GenderId = 2 и сортирует по GenderId и City
 select GenderId, City, sum(cast(Age as int)) as TotalAge, count(Id) as [Total Person(s)]
 from Person
 where GenderId = '2'
