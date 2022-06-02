b942f130ef32d3a92a7c4dfcbe1b267bcdf63400
rozhkov <artjom.rozhkov123@gmail.com>
2022-06-02 09:35:19 +0300

    select41.sql on loodud

---
+++ select41.sql
@@ -0,0 +1,8 @@
+﻿execute dbo.CalculateAge '10/08/2020'
+
+select * from EmployeesWithDates
+
+-- p�ring tabeli vastu, kus kuvatakse inimeste vanust
+select Id, Name, dbo.CalculateAge(DateOfBirth) as Age
+from EmployeesWithDates
+where dbo.CalculateAge(DateOfBirth) > 40
