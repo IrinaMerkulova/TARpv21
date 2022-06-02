a24820ab320cca8983606c774002d067490563e8
IljaTrunov <Iljatrunov123@gmail.com>
2022-06-02 09:28:53 +0300

    select20 - komenteeri

--- select20.sql
+++ select20.sql
@@ -1,2 +1,3 @@
-select City, GenderId, Sum(cast(Age as int)) as TotalAge from Person
+﻿select City, GenderId, Sum(cast(Age as int)) as TotalAge from Person
+--показывает город и сумму возрастов и группирует их по городу и ГендерИД и сортируя их по городам
 group by City, GenderId order by City
