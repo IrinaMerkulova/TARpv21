8ff4636521d8270edd8d432f0284ba9360a00bab
vlademirGužov <vlademirguzov@gmail.com>
2022-06-02 09:36:01 +0300

    select12-13 komenteeritud

--- Select12.sql
+++ Select12.sql
@@ -1,1 +1,2 @@
+--показать все из таблицы person сортировать в обратном порядке
 select * from Person order by Name desc
--- Select13.sql
+++ Select13.sql
@@ -1,1 +1,2 @@
+--показать первые 3 записи из таблицы peson
 select top 3 * from Person
