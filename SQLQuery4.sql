44e8e428d63b832b739c8d44a371ce66f9fdebd3
AlinaKolomoiets <alinakolomoits@gmail.com>
2022-06-02 09:22:03 +0300

    Select 4 City like n and Person like @

--- SQLQuery4.sql
+++ SQLQuery4.sql
@@ -1,2 +1,4 @@
+--Если в таблице City есть "n" то выводит название
 select * from Person where City like 'n%'
+--Если в таблице Person есть "@" то выводит название
 select * from Person where Email like '%@%'
