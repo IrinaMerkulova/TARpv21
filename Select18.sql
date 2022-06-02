5b32a0a53416102000daded98020ccf8bf7d6d83
Mihhail Lastovski <lastovskim@gmail.com>
2022-06-02 09:50:17 +0300

    Select 18 - kuvab vanuse summa linnade kaupa

--- Select18.sql
+++ Select18.sql
@@ -1,3 +1,4 @@
+--Выводит наибольший возраст
 select max(cast(Age as int)) from Person
-
+--Выводит сумму возраста по городам
 select City, sum(cast(Age as int)) as TotalAge from Person group by City
