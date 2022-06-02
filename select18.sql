34a42a2f3ecbb134427b20026010517c846cf496
IljaTrunov <Iljatrunov123@gmail.com>
2022-06-02 09:27:30 +0300

    select18 - komenteeri

--- select18.sql
+++ select18.sql
@@ -1,3 +1,5 @@
-select max(cast(Age as int)) from Person
+﻿select max(cast(Age as int)) from Person
+--максимальный возраст
 
 select City, sum(cast(Age as int)) as TotalAge from Person group by City
+--собирает сумму возрастом из городов и сортируя их по городам 
