cadb56f747f3e90ef3e6204cfe5264f2485aa860
ArturLink <arturlink04@gmail.com>
2022-06-02 09:44:06 +0300

    select 19 - sum vanus iga linnas

--- select19.sql
+++ select19.sql
@@ -1,3 +1,4 @@
+-- максимальный возраст
 select max(cast(Age as int)) from Person
-
+-- суммированный возраст в каждом городе
 select City, sum(cast(Age as int)) as TotalAge from Person group by City
