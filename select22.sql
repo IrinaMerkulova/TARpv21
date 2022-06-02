594757a9f39c81a279a0ce253cac6a35a6350b10
IljaTrunov <Iljatrunov123@gmail.com>
2022-06-02 09:30:25 +0300

    select22 - komenteeri

--- select22.sql
+++ select22.sql
@@ -1,4 +1,5 @@
-select GenderId, City, sum(cast(Age as int)) as TotalAge, count(Id) as [Total Person(s)]
+﻿select GenderId, City, sum(cast(Age as int)) as TotalAge, count(Id) as [Total Person(s)]
 from Person
 where GenderId = '2'
 group by GenderId, City
+--Высчитывает общий возраст из города и сколько человек понадобилось для общего возраста
