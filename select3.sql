6709ba8332a1c62828bd3846f562df9d5f7c9ca1
vlademirGužov <vlademirguzov@gmail.com>
2022-06-02 09:20:26 +0300

    select3 komenteeritud

--- select3.sql
+++ select3.sql
@@ -1,3 +1,5 @@
+--показать все ищ таблицы person где age равен 19 или 41 или 24
 select *from Person where Age = 19 or 
 Age = 41 or Age = 24
+--показать все из таблицы person где age равен 100 или 50 или 20
 select * from Person where Age in (100, 50, 20)
