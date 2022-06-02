df18f68c3acdd6640d476f39263906c8ac2cfd1a
DaniilKuzjomin <daniil.kuzjomin@gmail.com>
2022-06-02 09:17:54 +0300

    Select5 - linn algab N tähega, emailis on @ sümbol

--- select5.sql
+++ select5.sql
@@ -1,2 +1,4 @@
+-- выводит таблицу с условием, что город начинается на букву N
 select * from Person where City like 'n%'
+-- выводит таблицу с условием, что email содержит @
 select * from Person where Email like '%@%'
