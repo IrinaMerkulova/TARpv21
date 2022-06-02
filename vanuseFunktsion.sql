04f7e23640b15ec816ca8c6cda0b710a53efbe68
ArturLink <arturlink04@gmail.com>
2022-06-02 09:52:36 +0300

    vanuse funktsion

---
+++ vanuseFunktsion.sql
@@ -0,0 +1,16 @@
+create function dbo.CalculateAge (@DOB date)
+returns int
+as begin
+declare @Age int
+
+set @Age = datediff(year, @DOB, getdate()) - 
+	case
+		when (month(@DOB) > month(getdate())) or
+			 (month(@DOB) > month(getdate()) and 
+			 day(@DOB) > day(getdate()))
+		then 1
+		else 0
+		end
+	return @Age
+end
+
