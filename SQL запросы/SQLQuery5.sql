10df1baad9987e5c6d9c1447cbc1bdc020f8b6f2
Edgar Neverovski <edgarneverovskij@gmail.com>
2022-06-02 09:20:44 +0300

    Select5 - city algustähega n, email sümboliga @

--- SQL запросы/SQLQuery5.sql
+++ SQL запросы/SQLQuery5.sql
@@ -1,2 +1,4 @@
+--выводит все записи в Person где City начинается на "n"
 select * from Person where City like 'n%'
+--выводит все записи в Person где Email содержит "@"
 select * from Person where Email like '%@%'
