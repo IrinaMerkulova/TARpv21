1e56cdf3ac569a367772bbe69445a07b2f5b4a6a
rozhkov <artjom.rozhkov123@gmail.com>
2022-06-02 09:28:56 +0300

    select37.sql on loodud

--- select37.sql
+++ select37.sql
@@ -1,2 +1,8 @@
 select Id, Name, DateOfBirth, cast(DateOfBirth as nvarchar) as ConvertedDOB from EmployeesWithDates
 select Id, Name, DateOfBirth, convert(nvarchar, DateOfBirth) as ConvertedDOB from EmployeesWithDates
+
+select  cast(getdate() as date) --- n'itab t'nast kp-d
+select convert(date, getdate()) --- n'itab t'nast kp-d
+
+
+select Id, Name, Name + ' - ' + cast(Id as nvarchar) as [Name-Id] from EmployeesWithDates
