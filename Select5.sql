5ffe62d7b05e6ef6ae7e569b3b68469972ba2a08
vlademirGužov <vlademirguzov@gmail.com>
2022-06-02 09:27:57 +0300

    select5 komenteeritud

--- Select5.sql
+++ Select5.sql
@@ -1,2 +1,4 @@
+--показать все из таблицы person где city начинается на n
 select * from Person where City like 'n%'
+--показать все из таблицы person где email содержит @
 select * from Person where Email like '%@%'
